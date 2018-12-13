60592139e51df21db63e538334246584509e4a62
pravash <pravashupreti@gmail.com>
2018-12-13 21:57:42 +0545

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,5 +10,4 @@ parameter=$(python scripts/inliner.py \
     | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" \
     | tr '\r\n' ' ')
 
-sam deploy --template-file build/serverless-output.yaml --parameter-overrides RDSState="$RDS_STATE" BaseStack="$STACK_NAME" RDSSecurityGroup="$RDS_SECURITY_GROUP"  $parameter --stack-name $STACK_NAME-$2
-STACK_NAME
+sam deploy --template-file build/serverless-output.yaml --parameter-overrides RDSState="$RDS_STATE" BaseStack="$STACK_NAME" RDSSecurityGroup="$RDS_SECURITY_GROUP"  $parameter --stack-name $STACK_NAME-$1
