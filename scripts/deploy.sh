10c3e3d114dd236cfaf66af56e4369dd028725da
pravash <pravashupreti@gmail.com>
2019-01-07 15:15:46 +0545

    add-rest-api-id-stage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ parameter=$(python scripts/inliner.py \
     | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" \
     | tr '\r\n' ' ')
 
-aws cloudformation deploy --template-file sam-assets/template.yaml --parameter-overrides RDSState="$RDS_STATE" BaseStack="$STACK_NAME" RDSSecurityGroup="$RDS_SECURITY_GROUP"  $parameter --stack-name $STACK_NAME-$1-$MICRO_SERVICE_NAME
+aws cloudformation deploy --template-file sam-assets/template.yaml --parameter-overrides RDSState="$RDS_STATE" BaseStack="$STACK_NAME" RDSSecurityGroup="$RDS_SECURITY_GROUP"  $parameter --stack-name $STACK_NAME-$CI_COMMIT_REF_SLUG-$MICRO_SERVICE_NAME
 
 echo "Deploying the resources to default stage of"
 aws apigateway create-deployment --rest-api-id  $(aws cloudformation  describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`RestApiId`].OutputValue' --output text) --stage-name default --description 'Deployment to default stage'
