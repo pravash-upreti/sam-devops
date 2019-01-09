ebe5ed45ad79660d1f0c811a9b88d07952fe95d4
pravash <pravashupreti@gmail.com>
2019-01-09 14:37:07 +0545

    Refrence micro service name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,4 +13,4 @@ parameter=$(python scripts/inliner.py \
 aws cloudformation deploy --template-file sam-assets/template.yaml --parameter-overrides RDSState="$RDS_STATE" BaseStack="$STACK_NAME" RDSSecurityGroup="$RDS_SECURITY_GROUP"  $parameter --stack-name $STACK_NAME-$1-$MICRO_SERVICE_NAME
 STAGE=$1
 echo "Deploying the resources to default stage of"
-aws apigateway create-deployment --rest-api-id  $(aws cloudformation  describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`RestApiId$STAGE`].OutputValue' --output text) --stage-name default --description 'Deployment to default stage'
+aws apigateway create-deployment --rest-api-id  $(aws cloudformation  describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`RestApiId'$STAGE'`].OutputValue' --output text) --stage-name default --description 'Deployment to default stage'
