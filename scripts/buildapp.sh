9174c32cf6c48961f634bd8a9224cd1f4401e4d5
Pravash Upreti <pravashupreti@lftechnology.com>
2019-01-15 09:22:02 +0545

    log the client secret

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -2,6 +2,7 @@
 
 # move devops assets package json to new build dir
 
+echo curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app
 curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > $WSD/$APP_DIR/.env
 
 # rm -rf build_app
