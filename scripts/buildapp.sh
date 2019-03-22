64f49e9a0f653def1291a1a4d11ee3e9896842a0
Pravash Upreti <pravashupreti@lftechnology.com>
2019-03-22 17:21:18 +0545

    log the curl

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -3,8 +3,9 @@
 # move devops assets package json to new build dir
 
 echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app 
-curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/vyaguta/data/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > $WSD/$APP_DIR/.env
-cat $WSD/$APP_DIR/.env
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > $WSD/$APP_DIR/.env
+
 # rm -rf build_app
 # mkdir -p build_app
 
