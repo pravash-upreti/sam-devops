e5fcf47c7953b6c02dc925eb3581def43dc7d125
Pravash Upreti <pravashupreti@lftechnology.com>
2019-03-22 17:11:47 +0545

    Log the client credentials

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -4,7 +4,7 @@
 
 echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app 
 curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/vyaguta/data/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > $WSD/$APP_DIR/.env
-
+cat $WSD/$APP_DIR/.env
 # rm -rf build_app
 # mkdir -p build_app
 
