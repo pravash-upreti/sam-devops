426c0d3efcb483f23ae5a46b582bbcbd6b738b42
Pravash Upreti <pravashupreti@lftechnology.com>
2019-06-06 12:32:11 +0545

    Fix pipline issue in build_app.sh

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 # get frontend credentials from vault server
-curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app  \
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app | \
     
     # convert the response to json 
-    | jq .data.data \
+    jq .data.data | \
     
     # convert the json string to key value 
-    | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" \
+    jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" \
     
     # store the key value to .env file which is used during app build process
     > $WSD/$APP_DIR/.env
