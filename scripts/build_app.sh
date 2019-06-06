e9f0cd18ca67e3804b59bdf3805a3e79ef136667
Pravash Upreti <pravashupreti@lftechnology.com>
2019-06-06 14:43:54 +0545

    Restore old file

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,13 +1,24 @@
 #! /bin/sh
 
-# move devops assets package json to new build dir
+# get frontend credentials from vault server
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app | \
+    
+    # convert the response to json 
+    jq .data.data | \
+    
+    # convert the json string to key value 
+    jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" \
+    
+    # store the key value to .env file which is used during app build process
+    > $WSD/$APP_DIR/.env
 
-echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app 
-curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/$KV_ENGINE_NAME/data/$VAULT_SECRET_DIR/$STAGE/app | jq .data.data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > $WSD/$APP_DIR/.env
 cat $WSD/$APP_DIR/.env
-# rm -rf build_app
-# mkdir -p build_app
 
+# go to app directory
 cd $WSD/$APP_DIR
+
+# install the packages
 yarn
+
+# build the src
 yarn build
